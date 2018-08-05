2952096cd014854e28ac0d84a44d657b8f7aa96b
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-08-05 15:52:48 -0400

    add create request

--- router.go
+++ router.go
@@ -23,14 +23,45 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 
+
+func createRequest(apiurl string, reqBody interface{}, apimethod string) *http.Request {
+  jsonStr, err := json.Marshal(&reqBody)
+
+  if err != nil {
+  fmt.Fprintln(err)
+  return nil
+  }
+
+  req, e := http.NewRequest(apimethod, apiurl, bytes.NewBuffer(jsonStr))
+
+  if e != nil {
+    fmt.Fprintln(err)
+    return nil
+    }
+
+  return req
+}
+
 func getkeyvalue(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf("Hi getting key value")
 
-    // can add restful rputing here
+    allserverreq := make([]*http.Request, 0)
+
+    for i:=0; i < len(distributedserver); i++ {
+
+
+        getapi := fmt.Sprintf("http://%s:%d/getkv", distributedserver[i].Host, distributedserver[i].PortNum)
+
+        currentReq := createRequest(getapi, nil, http.MethodGet)
+        allserverreq = append(allserverreq, currentReq)
+    } // end of for
+
+    result, resultcode := requestServers(allserverreq)
+    sendResponse(w, r, result, resultcode)
 }
 
 func validate(e error) {
-if(e != nil) {
+if e != nil {
 fmt.Fprintf("Error in reading file %v", e)
 os.Exit(1)
 }
