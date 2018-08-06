fce6179554118ec3f1dded47393a90fbd018b44e
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-08-05 20:30:17 -0400

    compiling properly

--- router.go
+++ router.go
@@ -7,12 +7,24 @@ import (
 "io/ioutil"
 "os"
 "sync"
-"strings"
+"encoding/json"
+"bytes"
 )
 
 type distributedserver struct {
    Host string `json:"Host"`
    PortNum int `json:"PortNum"`
+}
+
+type Encoded struct {
+ Encoding string `json:"encoding"`
+ Data string `json:"data"`
+}
+
+
+type serverFetchResp struct {
+    Key Encoded `json:"key"`
+    Value *Encoded `json:"value"`
 }
 
 var keyValServer []distributedserver
@@ -30,19 +42,40 @@ func createRequest(apiurl string, reqBody interface{}, apimethod string) *http.R
   jsonStr, err := json.Marshal(&reqBody)
 
   if err != nil {
-  fmt.Fprintln(err)
+  // fmt.Fprintln(err)
   return nil
   }
 
   req, e := http.NewRequest(apimethod, apiurl, bytes.NewBuffer(jsonStr))
 
   if e != nil {
-    fmt.Fprintln(err)
+    // fmt.Fprintln(err)
     return nil
     }
 
   return req
 }
+
+
+// read the body and return. body is returned as byte array
+func loadRespBody(resp *http.Response) []byte {
+body, readErr := ioutil.ReadAll(resp.Body)
+
+if readErr != nil {
+log.Fatal(readErr)
+}
+
+return body
+}
+
+// takes the body as bytes , unmarshalls it and returns the struct
+func loadServerFetchResp(jsonBytes []byte) []serverFetchResp {
+var resps []serverFetchResp
+json.Unmarshal(jsonBytes, &resps)
+return resps
+}
+
+
 
 // this function takes responses from all distributed servers and concatenates all the key values into a single json
 func concatenateServerResp(resps []*http.Response) ([]byte, int) {
@@ -51,30 +84,31 @@ func concatenateServerResp(resps []*http.Response) ([]byte, int) {
  code := 200
 
 // loop over all response and concatenate json
- for _, response : range resps {
+ for _, response := range resps {
 
         if response.StatusCode >= 200 {
         body := loadRespBody(response)
         sresp := loadServerFetchResp(body)
 
         final = append(final, sresp...)
-       response.Body.Close()
+
         } // end of if
+         response.Body.Close()
+        } // end of for
 
         // the json body is encoded
         body, err := json.Marshal(final)
 
-        if e != nil {
-            fmt.Fprintln(err)
-            return nil
+        if err != nil {
+            // fmt.Fprintln(err)
+            return nil, 500
             }
 
         return body, code
 
- }
 }
 
-func requestServers(reqs []*http.Request) {
+func requestServers(reqs []*http.Request) ([]byte, int) {
 
      // when a server is requested it should be locked so that write does not occur at same time
      var mutex = &sync.Mutex{}
@@ -84,7 +118,7 @@ func requestServers(reqs []*http.Request) {
 
      wg.Add(len(reqs))
 
-     for _, curReq : range reqs {
+     for _, curReq := range reqs {
 
      go func(curReq *http.Request)   {
      defer wg.Done()
@@ -96,7 +130,7 @@ func requestServers(reqs []*http.Request) {
        } else {
        mutex.Lock()
        resps = append(resps, resp)
-       mutex.UnLock()
+       mutex.Unlock()
        }
 
      }(curReq)
@@ -109,15 +143,23 @@ func requestServers(reqs []*http.Request) {
 
 }
 
+
+// sends the response of all concat json value
+func sendResponse(w http.ResponseWriter, r *http.Request, reply []byte, code int) {
+w.Header().Set("Content-Type", "application/json")
+w.WriteHeader(code)
+w.Write(reply)
+}
+
 func getkeyvalue(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf("Hi getting key value")
+    // fmt.Fprintln("Hi getting key value")
 
     allserverreq := make([]*http.Request, 0)
 
-    for i:=0; i < len(distributedserver); i++ {
+    for i:=0; i < len(keyValServer); i++ {
 
 
-        getapi := fmt.Sprintf("http://%s:%d/getkv", distributedserver[i].Host, distributedserver[i].PortNum)
+        getapi := fmt.Sprintf("http://%s:%d/getkv", keyValServer[i].Host, keyValServer[i].PortNum)
 
         currentReq := createRequest(getapi, nil, http.MethodGet)
         allserverreq = append(allserverreq, currentReq)
@@ -129,7 +171,7 @@ func getkeyvalue(w http.ResponseWriter, r *http.Request) {
 
 func validate(e error) {
 if e != nil {
-fmt.Fprintf("Error in reading file %v", e)
+// fmt.Fprintf("Error in reading file %v", e)
 os.Exit(1)
 }
 }
